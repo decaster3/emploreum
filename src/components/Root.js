b5eb03e9570e8097ec3b4e884d0c566be43026f8
Rinat Katipov <rinat777hatipov@gmail.com>
2017-12-12 21:58:12 +0600

    add action

--- src/components/Root.js
+++ src/components/Root.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { ConnectedRouter } from 'react-router-redux'
 import configureRoutes from '../routes'
-import Provider from 'react-redux'
+import { Provider } from 'react-redux'
 
 class Root extends Component {
   render () {
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import App from './components/App'
-
+import MainDeveloperTestComponent from './components/main_developer_test_component'
 const configureRoutes = () => {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={App} />
+        <Route exact path='/developer_test' component={MainDeveloperTestComponent} />
       </Switch>
     </div>
   )
